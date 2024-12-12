238fab92bb04f56e2afb2f7949df02af1fccae37
JustOlie <oliverdonckers06@gmail.com>
2024-12-12 18:42:09 +0100

    Game script logica + verwijdering onnodige lijn met enums want die hebben geen associatie

--- src/Game.java
+++ src/Game.java
@@ -4,7 +4,24 @@ public class Game {
     Highscore highscore;
     Player player;
     PlayArea playArea;
-    Obstacles obstacles;
 
 
+    public Game(Timer timer, Highscore highscore, Player player, PlayArea playArea) {
+        this.timer = timer;
+        this.highscore = highscore;
+        this.player = player;
+        this.playArea = playArea;
+    }
+
+    public void startGame() {
+
+    }
+
+    public void endGame() {
+
+    }
+
+    public Highscore getHighscore() {
+        return highscore;
+    }
 }
