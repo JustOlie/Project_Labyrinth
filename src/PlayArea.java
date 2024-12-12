8c1b57162c310b8f8dab28f5ea5ede640fc40f44
JustOlie <oliverdonckers06@gmail.com>
2024-12-12 18:44:15 +0100

    Grid script logica om waardes aan toe te voegen

--- src/PlayArea.java
+++ src/PlayArea.java
@@ -1,5 +1,23 @@
 public class PlayArea {
 
+    private int[][] grid;
+
+    public PlayArea(int rows, int cols) {
+        grid = new int [rows][cols];
+        //GRID MET AANTAL RIJ EN KOLOM
+    }
+
+    public void setGridValue(int row, int col, int value) {
+        grid[row][col] = value;
+    }
+
+    public int getGridValue(int row, int col) {
+        return grid[row][col];
+    }
+
+    public int[][] getGrid() {
+        return grid;
+    }
 
 
 }
