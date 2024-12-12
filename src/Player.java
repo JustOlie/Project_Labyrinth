a5372dc4960ed85410884b835b089d0ae2dd048e
JustOlie <oliverdonckers06@gmail.com>
2024-12-12 17:37:26 +0100

    Aanpassing

--- src/Player.java
+++ src/Player.java
@@ -1,4 +1,33 @@
-package PACKAGE_NAME;
-
 public class Player {
+    private String name; // Spelernaam
+    private String difficulty; // Moeilijkheidsgraad
+
+    // Constructor
+    public Player(String name, String difficulty) {
+        this.name = name;
+        this.difficulty = difficulty;
+    }
+
+    // Getters en Setters
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDifficulty() {
+        return difficulty;
+    }
+
+    public void setDifficulty(String difficulty) {
+        this.difficulty = difficulty;
+    }
+
+    // toString Method
+    @Override
+    public String toString() {
+        return "Player{name='" + name + "', difficulty='" + difficulty + "'}";
+    }
 }
