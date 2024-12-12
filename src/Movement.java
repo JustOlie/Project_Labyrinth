53333a171531391d7c784c0897b255b09272da48
JustOlie <oliverdonckers06@gmail.com>
2024-12-12 18:26:41 +0100

    Test lines voor commiten verwijderd en Timer logic v1 gemaakt

--- src/Movement.java
+++ src/Movement.java
@@ -1,6 +1,4 @@
 public class Movement {
 
 
-
-    // Ik loop
 }
--- src/Timer.java
+++ src/Timer.java
@@ -1,13 +1,20 @@
 public class Timer {
 
-    long elapsedTime;
+    private long elapsedTime;
+    private long startTime;
+    private long stopTime;
+
 
     public void startTimer() {
-
+        startTime = System.currentTimeMillis();
+        elapsedTime = 0;
     }
 
     public void stopTimer() {
-
+        if (startTime != 0) { // DIT IS VOOR ZEKER TE ZIJN DAT DE ELAPSEDTIME STANDAARD 0 IS VOOR JUISTE BEREKENING.
+            elapsedTime = System.currentTimeMillis() - startTime;
+            startTime = 0; // RESET START TIMER VOOR VOLGENDE SPELER.
+        }
     }
 
     public long getElapsedTime() {
